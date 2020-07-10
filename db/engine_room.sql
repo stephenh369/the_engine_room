820dc3b596de1df9e4835b19d6691bfc16de0d40
Stephen <stephen_h369@live.co.uk>
2020-07-10 17:57:18 +0100

    setup sql file

---
+++ db/engine_room.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS manufacturers;
+
+CREATE TABLE manufacturers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    models VARCHAR(255)
+);
+
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY,
+    manufacturer INT REFERENCES manufacturers(id) ON DELETE CASCADE,
+    model VARCHAR(255),
+    condition VARCHAR(255),
+    type VARCHAR(255),
+    engine VARCHAR(255),
+    transmission VARCHAR(255),
+    fuel_type VARCHAR(255),
+    description VARCHAR(255),
+    stock INT,
+    buy_price INT,
+    sell_price INT
+);
