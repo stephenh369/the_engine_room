460483a7b16050eec4a1b00a917762ede9d65374
Stephen <stephen_h369@live.co.uk>
2020-07-11 16:01:36 +0100

    added some get/post methods to car_controller.rb

--- controllers/car_controller.rb
+++ controllers/car_controller.rb
@@ -4,3 +4,26 @@ require('pry-byebug')
 require_relative('../models/Car')
 require_relative('../models/Manufacturer')
 also_reload('../models/*')
+
+get '/cars' do
+    @cars = Car.all()
+    @manufacturers = Manufacturer.all()
+    erb(:"cars/index")
+end
+
+get '/cars/new' do
+    @cars = Car.all()
+    @manufacturers = Manufacturer.all()
+    erb(:"cars/new")
+end
+
+get '/cars/:id' do
+    @car = Car.find(params['id'].to_i)
+    erb(:"cars/show")
+end
+
+post '/cars' do
+    car = Car.new(params)
+    car.save
+    redirect to("/cars")
+  end
