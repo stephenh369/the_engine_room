ad39b795d8ce5cc9a65379c487d4e0abd65883df
Stephen <stephen_h369@live.co.uk>
2020-07-11 19:07:12 +0100

    added code to show.erb including spans for different stock quantities

--- controllers/car_controller.rb
+++ controllers/car_controller.rb
@@ -18,12 +18,24 @@ get '/cars/new' do
 end
 
 get '/cars/:id' do
-    @car = Car.find(params['id'].to_i)
+    @car = Car.find(params[:id].to_i)
+    if @car.stock == 0
+        @span1 = "Out Of Stock"
+    elsif @car.stock == 1
+        @span2 = "Available"
+    elsif @car.stock > 1
+        @span3 = "More Than 1 Available"
+    end
     erb(:"cars/show")
 end
 
 post '/cars' do
     car = Car.new(params)
     car.save
-    redirect to("/cars")
-  end
+    redirect "/cars"
+end
+
+post '/cars/:id/delete' do
+    Car.delete(params[:id])
+    redirect "/cars"
+end
