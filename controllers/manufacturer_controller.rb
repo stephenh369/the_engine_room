e25c04e7ea2bd1915b6f8f21f2a14334d427cadf
Stephen <stephen_h369@live.co.uk>
2020-07-12 10:47:55 +0100

    added manufacturer erb pages and get methods

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -1,6 +1,20 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require('pry-byebug')
 require_relative('../models/Manufacturer')
 require_relative('../models/Car')
 also_reload('../models/*')
+
+get '/manufacturers' do
+    @manufacturers = Manufacturer.all()
+    erb(:"manufacturers/index")
+end
+
+get '/manufacturers/new' do
+    @manufacturers = Manufacturer.all()
+    erb(:"manufacturers/new")
+end
+
+get '/manufacturers/:id' do
+    @manufacturer = Manufacturer.find(params[:id].to_i)
+    erb(:"manufacturers/show")
+end
